a0f831c92494dc5c8d281d1684398b0194a3bedf
athinodor <athinodor@gmail.com>
2026-04-13 20:38:34 +0300

    Add files via upload

---
+++ VisualBasic/SaveArduinoOutput/SaveArduinoOutput/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSaveArduinoData
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnStart = New Button()
+        btnStop = New Button()
+        txtData = New TextBox()
+        btnCloseApp = New Button()
+        Label1 = New Label()
+        cmbPort = New ComboBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtFileName = New TextBox()
+        Label4 = New Label()
+        fbdSaveFolder = New FolderBrowserDialog()
+        btnSelectFolder = New Button()
+        txtSaveFolder = New TextBox()
+        btnCreateFile = New Button()
+        btnClearFileData = New Button()
+        SuspendLayout()
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(12, 143)
+        btnStart.Margin = New Padding(3, 4, 3, 4)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(158, 40)
+        btnStart.TabIndex = 0
+        btnStart.Text = "Έναρξη Καταγραφής"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' btnStop
+        ' 
+        btnStop.Location = New Point(176, 143)
+        btnStop.Margin = New Padding(3, 4, 3, 4)
+        btnStop.Name = "btnStop"
+        btnStop.Size = New Size(173, 40)
+        btnStop.TabIndex = 1
+        btnStop.Text = "Διακοπή Καταγραφής"
+        btnStop.UseVisualStyleBackColor = True
+        ' 
+        ' txtData
+        ' 
+        txtData.Location = New Point(3, 222)
+        txtData.Margin = New Padding(3, 4, 3, 4)
+        txtData.Multiline = True
+        txtData.Name = "txtData"
+        txtData.ScrollBars = ScrollBars.Vertical
+        txtData.Size = New Size(340, 479)
+        txtData.TabIndex = 2
+        ' 
+        ' btnCloseApp
+        ' 
+        btnCloseApp.Location = New Point(400, 279)
+        btnCloseApp.Margin = New Padding(3, 4, 3, 4)
+        btnCloseApp.Name = "btnCloseApp"
+        btnCloseApp.Size = New Size(158, 40)
+        btnCloseApp.TabIndex = 3
+        btnCloseApp.Text = "Τερματισμός"
+        btnCloseApp.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(3, 198)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(134, 20)
+        Label1.TabIndex = 4
+        Label1.Text = "Δεδομένα Arduino"
+        ' 
+        ' cmbPort
+        ' 
+        cmbPort.DisplayMember = "COM1"
+        cmbPort.DropDownStyle = ComboBoxStyle.DropDownList
+        cmbPort.FormattingEnabled = True
+        cmbPort.Items.AddRange(New Object() {"COM1", "COM2", "COM3", "COM4", "COM5", "COM6"})
+        cmbPort.Location = New Point(176, 76)
+        cmbPort.Margin = New Padding(3, 4, 3, 4)
+        cmbPort.Name = "cmbPort"
+        cmbPort.Size = New Size(138, 28)
+        cmbPort.TabIndex = 5
+        cmbPort.ValueMember = "COM1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(3, 79)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(111, 20)
+        Label2.TabIndex = 6
+        Label2.Text = "Σειριακή Θύρα:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(3, 41)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(156, 20)
+        Label3.TabIndex = 7
+        Label3.Text = "Αρχείο αποθήκευσης:"
+        ' 
+        ' txtFileName
+        ' 
+        txtFileName.Location = New Point(176, 41)
+        txtFileName.Margin = New Padding(3, 4, 3, 4)
+        txtFileName.Name = "txtFileName"
+        txtFileName.Size = New Size(139, 27)
+        txtFileName.TabIndex = 8
+        txtFileName.Text = "arduino_data.csv"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(3, 9)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(167, 20)
+        Label4.TabIndex = 9
+        Label4.Text = "Φάκελος αποθήκευσης:"
+        ' 
+        ' btnSelectFolder
+        ' 
+        btnSelectFolder.Location = New Point(556, 4)
+        btnSelectFolder.Margin = New Padding(3, 4, 3, 4)
+        btnSelectFolder.Name = "btnSelectFolder"
+        btnSelectFolder.Size = New Size(40, 31)
+        btnSelectFolder.TabIndex = 10
+        btnSelectFolder.Text = "..."
+        btnSelectFolder.UseVisualStyleBackColor = True
+        ' 
+        ' txtSaveFolder
+        ' 
+        txtSaveFolder.Location = New Point(176, 7)
+        txtSaveFolder.Name = "txtSaveFolder"
+        txtSaveFolder.Size = New Size(374, 27)
+        txtSaveFolder.TabIndex = 11
+        ' 
+        ' btnCreateFile
+        ' 
+        btnCreateFile.Location = New Point(339, 79)
+        btnCreateFile.Name = "btnCreateFile"
+        btnCreateFile.Size = New Size(190, 29)
+        btnCreateFile.TabIndex = 12
+        btnCreateFile.Text = "Δημιουργία Αρχείου"
+        btnCreateFile.UseVisualStyleBackColor = True
+        ' 
+        ' btnClearFileData
+        ' 
+        btnClearFileData.Location = New Point(355, 143)
+        btnClearFileData.Name = "btnClearFileData"
+        btnClearFileData.Size = New Size(174, 40)
+        btnClearFileData.TabIndex = 13
+        btnClearFileData.Text = "Καθάρισμα Αρχείου"
+        btnClearFileData.UseVisualStyleBackColor = True
+        ' 
+        ' frmSaveArduinoData
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(598, 702)
+        Controls.Add(btnClearFileData)
+        Controls.Add(btnCreateFile)
+        Controls.Add(txtSaveFolder)
+        Controls.Add(btnSelectFolder)
+        Controls.Add(Label4)
+        Controls.Add(txtFileName)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(cmbPort)
+        Controls.Add(Label1)
+        Controls.Add(btnCloseApp)
+        Controls.Add(txtData)
+        Controls.Add(btnStop)
+        Controls.Add(btnStart)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "frmSaveArduinoData"
+        Text = "Save Arduino Data"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents txtData As TextBox
+    Friend WithEvents btnCloseApp As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbPort As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtFileName As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents fbdSaveFolder As FolderBrowserDialog
+    Friend WithEvents btnSelectFolder As Button
+    Friend WithEvents txtSaveFolder As TextBox
+    Friend WithEvents btnCreateFile As Button
+    Friend WithEvents btnClearFileData As Button
+
+End Class
---
+++ VisualBasic/SaveArduinoOutput/SaveArduinoOutput/Form1.vb
@@ -0,0 +1,182 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class frmSaveArduinoData
+    Dim WithEvents SerialPort1 As New SerialPort
+    Dim filePath As String '= "C:\Users\3lykpyrgportable1\OneDrive\Έγγραφα\arduino_data.csv" ' "C:\arduino_data.csv"
+    Dim loggingEnabled As Boolean = False
+    Private Sub frmSaveArduinoData_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Ρύθμιση σειριακής θύρας (άλλαξε COM ανάλογα με το Arduino σου)
+        cmbPort.SelectedIndex = 3
+        SerialPort1.PortName = cmbPort.Text
+        SerialPort1.BaudRate = 9600
+
+
+
+
+
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Try
+            If Not SerialPort1.IsOpen Then
+                SerialPort1.Open()
+            End If
+
+            loggingEnabled = True
+            txtData.AppendText("▶ Καταγραφή ξεκίνησε..." & Environment.NewLine)
+        Catch ex As Exception
+            MessageBox.Show("Σφάλμα: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        loggingEnabled = False
+        txtData.AppendText("■ Καταγραφή σταμάτησε." & Environment.NewLine)
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Try
+            Dim line As String = SerialPort1.ReadLine().Trim()
+
+            If loggingEnabled Then
+                Dim output As String = $"{line}" '$"{DateTime.Now},{line}"
+                File.AppendAllText(filePath, output & Environment.NewLine)
+
+                Me.Invoke(Sub()
+                              txtData.AppendText(output & Environment.NewLine)
+                          End Sub)
+            End If
+        Catch ex As Exception
+            ' αγνόησε σφάλματα ανάγνωσης
+        End Try
+    End Sub
+
+    Private Sub frmSaveArduinoData_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If SerialPort1.IsOpen Then SerialPort1.Close()
+    End Sub
+
+    Private Sub btnSelectFolder_Click(sender As Object, e As EventArgs) Handles btnSelectFolder.Click
+
+        Using dlg As New FolderBrowserDialog()
+            dlg.Description = "Επιλέξτε φάκελο αποθήκευσης"
+            dlg.ShowNewFolderButton = True
+
+            ' Προεπιλογή στον φάκελο του current filePath αν υπάρχει
+            Try
+                Dim currentDir As String = If(String.IsNullOrWhiteSpace(txtSaveFolder.Text),
+                                              Path.GetDirectoryName(filePath),
+                                              txtSaveFolder.Text)
+                If Not String.IsNullOrWhiteSpace(currentDir) AndAlso Directory.Exists(currentDir) Then
+                    dlg.SelectedPath = currentDir
+                End If
+            Catch
+                ' αγνόησε τυχόν σφάλματα στην ανάγνωση του path
+            End Try
+
+            If dlg.ShowDialog() = DialogResult.OK Then
+                txtSaveFolder.Text = dlg.SelectedPath
+            End If
+        End Using
+
+    End Sub
+
+
+    Private Sub cmbPort_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbPort.SelectedIndexChanged
+        SerialPort1.PortName = cmbPort.Text
+    End Sub
+
+    Private Sub btnCreateFile_Click(sender As Object, e As EventArgs) Handles btnCreateFile.Click
+        filePath = txtSaveFolder.Text & "\" & txtFileName.Text
+        If filePath = "\" Then
+            MsgBox("Επιλέξτε φάκελο και όνομα αρχείου για αποθήκευση δεδομένων.", MsgBoxStyle.Exclamation, "Προσοχή")
+        Else
+
+            ' Δημιουργία αρχείου αν δεν υπάρχει
+            If Not File.Exists(filePath) Then
+                'File.WriteAllText(filePath, "Χρόνος,Απόσταση (cm)" & Environment.NewLine)
+                File.WriteAllText(filePath, "t (s),x (cm),V (cm/s),a (cm/s^2)" & Environment.NewLine)
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub btnCloseApp_Click(sender As Object, e As EventArgs) Handles btnCloseApp.Click
+        ' Αν υπάρχει ενεργή καταγραφή, ρώτησε τον χρήστη
+        If loggingEnabled Then
+            Dim res As DialogResult = MessageBox.Show("Υπάρχει ενεργή καταγραφή. Να σταματήσει η καταγραφή και να κλείσει η εφαρμογή;", "Επιβεβαίωση", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If res = DialogResult.No Then
+                Return
+            End If
+        End If
+
+        ' Σιγουρέψου ότι η καταγραφή σταμάτησε και η θύρα έκλεισε
+        loggingEnabled = False
+        Try
+            If SerialPort1 IsNot Nothing AndAlso SerialPort1.IsOpen Then
+                SerialPort1.Close()
+            End If
+        Catch ex As Exception
+            ' Μπορείς να το καταγράψεις αν θες, αλλά δεν είναι κρίσιμο
+        End Try
+
+        ' Κλείσιμο της φόρμας (θα τρέξει το FormClosing handler)
+        Me.Close()
+        ' Εναλλακτικά, αν θέλεις να βεβαιωθείς ότι τερματίζει όλη η εφαρμογή:
+        ' Application.Exit()
+    End Sub
+
+    Private Sub btnClearFileData_Click(sender As Object, e As EventArgs) Handles btnClearFileData.Click
+        filePath = txtSaveFolder.Text & "\" & txtFileName.Text
+        If filePath = "\" Then
+            MsgBox("Επιλέξτε φάκελο και όνομα αρχείου για αποθήκευση δεδομένων.", MsgBoxStyle.Exclamation, "Προσοχή")
+        Else
+
+            ' Επιβεβαίωση
+            If loggingEnabled Then
+                Dim ans As DialogResult = MessageBox.Show("Υπάρχει ενεργή καταγραφή. Να σταματήσει και να καθαριστεί το αρχείο;", "Επιβεβαίωση", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                If ans = DialogResult.No Then Return
+            End If
+
+            Dim wasLogging As Boolean = loggingEnabled
+            Dim portWasOpen As Boolean = False
+
+            ' Σταμάτα καταγραφή και κλείσε θύρα προσωρινά
+            loggingEnabled = False
+            Try
+                portWasOpen = (SerialPort1 IsNot Nothing AndAlso SerialPort1.IsOpen)
+                If portWasOpen Then SerialPort1.Close()
+            Catch
+                ' αγνόησε σφάλματα στο κλείσιμο της θύρας
+            End Try
+
+            Try
+                ' Αν θέλεις να διαγράψεις το αρχείο εντελώς:
+                ' If File.Exists(filePath) Then File.Delete(filePath)
+
+                ' Εδώ: αντικαθιστούμε το περιεχόμενο κρατώντας header
+                Dim header As String = "t (s),x (cm),V (cm/s),a (cm/s^2)" & Environment.NewLine
+                File.WriteAllText(filePath, header)
+
+                txtData.AppendText("■ Το αρχείο καθαρίστηκε: " & filePath & Environment.NewLine)
+            Catch ex As Exception
+                MessageBox.Show("Σφάλμα κατά το καθάρισμα αρχείου: " & ex.Message, "Σφάλμα", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Finally
+                ' Προσπαθούμε να επαναφέρουμε τη θύρα και την κατάσταση καταγραφής
+                Try
+                    If portWasOpen Then SerialPort1.Open()
+                Catch ex As Exception
+                    MessageBox.Show("Δεν ήταν δυνατή η επαναφορά της σειριακής θύρας: " & ex.Message, "Προσοχή", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                End Try
+                loggingEnabled = wasLogging
+            End Try
+
+        End If
+
+
+
+
+    End Sub
+End Class
